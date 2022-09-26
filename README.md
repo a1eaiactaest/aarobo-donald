f4853ca2ab62caed99d989564f3a9fa3b95a882a
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-09-26 17:44:34 +0200

    going to the bigger machine

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 # Aarobo Donald
 
 The biggest and scariest sumo robot at BSR.
+
+
+## Electronics
+
+### Arduino Shield
+
+* [Datasheet](https://5.imimg.com/data5/PX/UK/MY-1833510/l293d-based-arduino-motor-shield.pdf)
+  - up to 4 motors
+
+### Ultrasonic Ranging
+
+* [Datasheet](https://cdn.sparkfun.com/datasheets/Sensors/Proximity/HCSR04.pdf)
+
+
+### Reflective Optical Sensor
+
+* [Datasheet](https://www.vishay.com/docs/83751/cny70.pdf)
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,57 @@
 #include <Arduino.h>
+#include <AFMotor.h>
+
+#define PIN_OPT_SENSOR A0
+
+#define PIN_RANGE_ECHO 2
+#define PIN_RANGE_TRIGGER 3
+
+int calc_centrimeters(long duration){
+  /**
+   * @brief Description of calc_centimeters function.
+   * @param duration The length of the pulse (in microseconds) or 0 if no pulse started before the timeout. Data type: unsigned long.
+   * @return distance in centimeters.
+   */
+
+  return (duration/2) * 0.0343;
+}
+
+void clear_trigPIN(){
+  digitalWrite(PIN_RANGE_TRIGGER, LOW);
+}
+
+int get_distance(){
+  /**
+   * @brief Get distance from base to the object.
+   * @return centimeters to the object.
+   */
+
+  clear_trigPIN();
+
+  digitalWrite(PIN_RANGE_TRIGGER, HIGH);
+  delay(10);
+  digitalWrite(PIN_RANGE_TRIGGER, LOW);
+
+  long duration = pulseIn(PIN_RANGE_ECHO, HIGH);
+  int distance = calc_centrimeters(duration);
+
+  return distance;
+}
+
+int optical_sensor(){
+  int sensor_val = analogRead(PIN_OPT_SENSOR);
+  return sensor_val;
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(PIN_RANGE_ECHO, INPUT);
+  pinMode(PIN_RANGE_TRIGGER, OUTPUT);
+
+  Serial.begin(9600);
+  Serial.println("Serial init");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println(optical_sensor());
+  delay(1000);
 }
